0c380b43927c1c4460d336dcd715179843907c31
Tyler Degand <tylerdegand@gmail.com>
2020-07-27 16:08:31 -0500

    getting closer. Need to edit some things to get the content to dynamically generate

--- app.js
+++ app.js
@@ -8,8 +8,13 @@ app.use('/static', express.static('public'))
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-    const indexData = { project }
-    res.render('index', indexData)
+
+    project.forEach(project => {
+        const photos = JSON.parse(project.image_urls)
+        const photo1 = photos[0];
+    })
+
+    res.render('index', { photo1 })
 })
 
 app.get('/about', (req, res) => {
@@ -24,13 +29,14 @@ app.get('/project/:id', (req, res) => {
         const tech = project.technologies
         const live = project.live_link
         const github = project.github_link
-        const photos = project.image_urls
-        const projectData = { name, description, tech, live, github, photos }
 
-        return projectData
+        const photos = JSON.parse(project.image_urls)
+        const photo2 = photos[1]
+        const photo3 = photos[2]
+
+        res.render('project', { name, description, tech, live, github, photo2, photo3 });
     });
 
-    res.render('project', projectData);
 })
 
 app.listen(3000);
