534770ad0d36c24ba3097b3fa0456ec28e294a3c
Tyler Degand <tylerdegand@gmail.com>
2020-07-27 12:49:39 -0500

    rendering index page and about page now

--- app.js
+++ app.js
@@ -2,4 +2,16 @@ const express = require('express');
 const data = require('./data.json');
 const app = express();
 
+app.use('/static', express.static('public'))
+
+app.set('view engine', 'pug')
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
 app.listen(3000);
