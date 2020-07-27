7823a0c2af134b2067e7129153d7c8c3a84a3b46
Tyler Degand <tylerdegand@gmail.com>
2020-07-27 14:12:00 -0500

    made some changes to this

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const data = require('./data.json');
+const { project } = require('./data/data.json');
+
 const app = express();
 
 app.use('/static', express.static('public'))
@@ -7,11 +8,26 @@ app.use('/static', express.static('public'))
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-    res.render('index')
+    const indexData = { project }
+    res.render('index', indexData)
 })
 
 app.get('/about', (req, res) => {
     res.render('about')
+})
+
+app.get('/project/:id', (req, res) => {
+    const id = project.id
+    const name = project.name
+    const description = project.description
+    const tech = project.technologies
+    const live = project.live_link
+    const github = project.github_link
+    const photos = project.image_urls
+
+    const projectData = { name, description, tech, live, github, photos }
+
+    res.render('project', projectData);
 })
 
 app.listen(3000);
