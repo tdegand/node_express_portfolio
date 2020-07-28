85defbc2d9450097cc2a668cdb55b17a02e678f3
Tyler Degand <tylerdegand@gmail.com>
2020-07-28 07:54:37 -0500

    Made some updates

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { project } = require('./data/data.json');
+const { projects } = require('./data/data.json');
 
 const app = express();
 
@@ -8,13 +8,7 @@ app.use('/static', express.static('public'))
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-
-    project.forEach(project => {
-        const photos = JSON.parse(project.image_urls)
-        const photo1 = photos[0];
-    })
-
-    res.render('index', { photo1 })
+    res.render('index', { projects })
 })
 
 app.get('/about', (req, res) => {
@@ -22,20 +16,19 @@ app.get('/about', (req, res) => {
 })
 
 app.get('/project/:id', (req, res) => {
+    const projectId = req.params.id;
+    const project = projects.find(({ id }) => id === +projectId);
+    const newLocal = JSON.parse(project.image_urls);
+    const photos = newLocal
+    const photo1 = photos[0]
+    const photo2 = photos[1]
+    const photo3 = photos[2]
+    if (project) {
+        res.render('project', { project, photo1, photo2, photo3 })
+    } else {
+        res.sendStatus(404);
+    }
 
-    project.forEach(project => {
-        const name = project.project_name
-        const description = project.description
-        const tech = project.technologies
-        const live = project.live_link
-        const github = project.github_link
-
-        const photos = JSON.parse(project.image_urls)
-        const photo2 = photos[1]
-        const photo3 = photos[2]
-
-        res.render('project', { name, description, tech, live, github, photo2, photo3 });
-    });
 
 })
 
