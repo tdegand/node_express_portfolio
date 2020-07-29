a71d6606fc5c063e9a7c3cdcec2921c17445e21c
Tyler Degand <tylerdegand@gmail.com>
2020-07-29 11:09:21 -0500

    updates files

--- routes.js
+++ routes.js
@@ -8,10 +8,12 @@ const { projects } = require('./data/data.json');
 router.get('/', (req, res) => {
     res.render('index', { projects })
 })
+
 //about page route
 router.get('/about', (req, res) => {
     res.render('about')
 })
+
 //projects page routes
 //dynamically pulls in the proper page when project is selected
 router.get('/project/:id', (req, res, next) => {
@@ -20,12 +22,14 @@ router.get('/project/:id', (req, res, next) => {
     if (project) {
         res.render('project', { project })
     } else {
+        //handles any errors outside of this route
         const error = new Error("This page cannot be found")
         error.status = 404
         next(error)
     }
 });
 
+//handles any other errors that may occur in routing
 router.get('*', (req, res, next) => {
     const error = new Error("This page cannot be found")
     error.status = 404
